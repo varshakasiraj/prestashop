5aabd64a16dfab11cfa7815750ae9fb149aecbf8
varshakasiraj <116131392+varshakasiraj@users.noreply.github.com>
2023-07-18 18:12:48 +0530

    adjusting slick slider (insert tdb operation)

--- shashop/modules/sp_banner/sp_banner.php
+++ shashop/modules/sp_banner/sp_banner.php
@@ -31,20 +31,71 @@ class Sp_Banner extends Module{
         $this->context->controller->registerJavascript('sp_banner_js', '/modules/sp_banner/js/custome.js', ['position' => 'bottom', 'priority' => 1000]);
     }
     public function hookDisplayHome(){
+        $this->context->smarty->assign(
+            [
+                'slider' => $this->selectSlider(),
+            ]
+    );
         return  $this->display(__FILE__, 'sp_banner.tpl');
     }
     public function getContent(){
         $output = '';
-        if(!empty("input_slider") && Tools::isSubmit('submit'.$this->name))
+        if(Tools::isSubmit('input_slider'.$this->name))
         {
-            $check = (string) Tools::getValue("input_slider");
-            var_dump($check);
-             $module_name = (string) Tools::getValue("slider_name");
-             var_dump($module_name);
-             Configuration::updateValue("slider_name",$module_name);
+             $module_name = (string) Tools::getValue("block_name");
+             Configuration::updateValue("block_name",$module_name);
              $output = $this->displayConfirmation($this->l('Settings updated'));
         }
+        elseif(Tools::isSubmit('insert_slider'.$this->name))
+        {
+            $image = (string) Tools::getValue("image_name");
+            $image_name = $this->imageValidation($image);
+            $slider_name= (string) Tools::getValue("slider_name");
+            $path = (string) Tools::getValue("link");
+            $positions = (int) Tools::getValue("position");
+            $status = (int) Tools::getValue("status");
+            $output = $this->displayConfirmation($this->l('Sucessfully Filled the Fileds'));
+            $data =[
+                'image_name'=>$image_name,
+                'slider_name'=>$slider_name,
+                'path'=>$path,
+                'status'=>$status,
+                'positions'=>$positions,
+            ];
+           return $this->insertSlider($data);
+        }
         return $output.$this->displayForm().$this->displayInsertForm();
+    }
+    public function insertSlider($data){
+        $db = \Db::getInstance();
+        $insert = $db->insert('sp_banner',$data,Db::INSERT);
+        return $insert;
+    }
+    public function selectSlider(){
+        $db = \Db::getInstance();
+        $request = 'SELECT * FROM ps_sp_banner';
+        $result = $db->executeS($request);
+        return $result;
+    }
+    public function getErrors(){
+        $errors = [
+            "image"=>"Wheather image is empty or having wrong extension",
+        ];
+    }
+    public function imageValidation($image){
+        $allowed_img_extension = ["jpg", "jpeg", "bmp", "gif", "png","jfif"];
+        $basename =  basename($image);
+        $extension = substr(strrchr($basename, '.'), 1);
+        if (!empty($image)&& in_array($extension, $allowed_img_extension)) {
+            $path = $_FILES['image_name']['tmp_name'];
+            $image_path = dirname(__DIR__)."\sp_banner\image\\".$basename;
+            if(move_uploaded_file($path, $image_path)){
+                return $basename;
+            }
+        } 
+        else {
+            echo "Wheather image is empty or having wrong extension";
+        }
     }
     public function displayForm(){
         $form=[
@@ -52,72 +103,94 @@ class Sp_Banner extends Module{
                 'input' => [
                     array(
                         'type' => 'text',
-                        'name' => 'slider_name',
+                        'name' => 'block_name',
                         'required' => true,
-                        'label' =>'Module_Name'
+                        'label' =>'Enter your Block Name'
                     ),
-                    array(
-                        'type' => 'hidden',
-                        'name' => 'input_slider',
-                        'required' => true,        
-                    ),
-                    
-                    
                 ],
                 'submit' =>[
                     'title' => 'submit',
+                    'name' =>'input_slider',
                 ]
             ]
            
         ];
         $helperform = new HelperForm();
         $helperform->table = $this->table;
-        $helperform->name_controller = $this->name;
-        $helperform->submit_action = 'submit' . $this->name;
-        $helperform->fields_value['slider_name'] = Configuration::get("slider_name");
-        $helperform->fields_value['input_slider'] = 'test';
+        $helperform->submit_action = 'input_slider' . $this->name;
+        $helperform->fields_value['block_name'] = Configuration::get("block_name");
         return $helperform->generateForm([$form]);
+    }
+    public function data(){
+        $data =[];
     }
     public function displayInsertForm(){
         $form=[
             'form' =>[
                 'input' =>array(
                     array(
+                        'type' =>"file",
+                        'name' => 'image_name',
+                        'required' => true,
+                        'label' =>'Slider Name'
+                    ),
+                    array(
                         'type' => 'text',
                         'name' => 'slider_name',
                         'required' => true,
-                        'label' =>'Module_Name'
+                        'label' =>'Slider Name'
                     ),
                     array(
                         'type'=>'text',
-                        'name' => 'title',
+                        'name' => 'link',
                         'required' => true,
-                        'label' =>'title'
+                        'label' =>'Link'
                     ),
                     array(
                         'type'=>'text',
                         'name' => 'position',
                         'required' => true,
-                        'label' =>'position'
+                        'label' =>'Position'
                     ),
                     array(
-                        'type'=>'text',
+                        'type'=>'radio',
                         'name' => 'status',
                         'required' => true,
-                        'label' =>'status'
+                        'label' =>'Status',
+                        'isbool' =>true,
+                        'values' => array(
+                            array(
+                                'id' =>'enable',
+                                'value' =>1,
+                                'label' =>'Enabled',
+                              ),
+                            array(
+                              'id' =>'disable',
+                              'value' =>0,
+                              'label' =>'Disabled',
+                              
+                            ),
+                        )
                     ),
 
                 ),
                 'submit' =>[
-                    'title' => 'submit',
+                    'title' => 'Insert',
+                    'name' => 'insert_slider'
                 ]
             ]
            
         ];
         $helperform = new HelperForm();
         $helperform->table = $this->table;
-        $helperform->name_controller = $this->name;
-        $helperform->submit_action = 'submit' . $this->name;
+
+        $helperform->submit_action = 'insert_slider' . $this->name;
+
+        $helperform->fields_value['image_name'] = Tools::getValue("image_name");
+        $helperform->fields_value['slider_name'] = Tools::getValue("slider_name");
+        $helperform->fields_value['link'] = Tools::getValue("link");
+        $helperform->fields_value['position'] = Tools::getValue("position");
+        $helperform->fields_value['status'] = Tools::getValue("status");
         return $helperform->generateForm([$form]);
     }
     
--- shashop/modules/sp_banner/sp_banner.tpl
+++ shashop/modules/sp_banner/sp_banner.tpl
@@ -1,40 +1,16 @@
 <div class="sp_container">
     <div class="sp_wrapper">
-        <div class="sp_card">
-            <div class="sp_image">
-                <img src="/prestashop/shashop/modules/sp_banner/image/coffeemug.jpg"/>
-            </div>
-            <div class="sp_description 1">
-                <h5>New Arrivals Coffee mug </h5>
-                <button>Shop now</button>
-            </div>
-        </div>
-        <div class="sp_card">
-            <div class="sp_image">
-                <img src="/prestashop/shashop/modules/sp_banner/image/shoes.jpg"/>
-            </div>
-            <div class="sp_description">
-                <h5>New Arrivals Coffee mug </h5>
-                <button>Shop now</button>
-            </div>
-        </div>
-        <div class="sp_card">
-            <div class="sp_image">
-                <img src="/prestashop/shashop/modules/sp_banner/image/clothes.jpg"/>
-            </div>
-            <div class="sp_description">
-                <h5>New Arrivals Coffee mug </h5>
-                <button>Shop now</button>
-            </div>
-        </div>
-        <div class="sp_card">
-            <div class="sp_image">
-                <img src="/prestashop/shashop/modules/sp_banner/image/photoframe.jpg"/>
-            </div>
-            <div class="sp_description">
-                <h5>New Arrivals Coffee mug </h5>
-                <button class="sp_button">Shop now</button>
-            </div>
-        </div>
+        {{foreach from=collection item=item key=key name=name}}
+            <div class="sp_card one">
+                    <div class="sp_image">
+                        <img src="/prestashop/shashop/modules/sp_banner/image/"{}/>
+                    </div>
+                    <div class="sp_description 1">
+                        <h5></h5>
+                        <button>Shop now</button>
+                    </div>
+                </div>
+            
+        {{/foreach}}
     </div>
 </div>
