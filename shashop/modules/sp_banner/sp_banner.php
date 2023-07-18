5a83ad3259c89683186b5cf54c0e513f2f673c1d
varshakasiraj <116131392+varshakasiraj@users.noreply.github.com>
2023-07-18 10:51:52 +0530

    Configure Custome modules
    creating form and then insert data (in progress)

--- shashop/modules/sp_banner/sp_banner.php
+++ shashop/modules/sp_banner/sp_banner.php
@@ -33,5 +33,93 @@ class Sp_Banner extends Module{
     public function hookDisplayHome(){
         return  $this->display(__FILE__, 'sp_banner.tpl');
     }
+    public function getContent(){
+        $output = '';
+        if(!empty("input_slider") && Tools::isSubmit('submit'.$this->name))
+        {
+            $check = (string) Tools::getValue("input_slider");
+            var_dump($check);
+             $module_name = (string) Tools::getValue("slider_name");
+             var_dump($module_name);
+             Configuration::updateValue("slider_name",$module_name);
+             $output = $this->displayConfirmation($this->l('Settings updated'));
+        }
+        return $output.$this->displayForm().$this->displayInsertForm();
+    }
+    public function displayForm(){
+        $form=[
+            'form' =>[
+                'input' => [
+                    array(
+                        'type' => 'text',
+                        'name' => 'slider_name',
+                        'required' => true,
+                        'label' =>'Module_Name'
+                    ),
+                    array(
+                        'type' => 'hidden',
+                        'name' => 'input_slider',
+                        'required' => true,        
+                    ),
+                    
+                    
+                ],
+                'submit' =>[
+                    'title' => 'submit',
+                ]
+            ]
+           
+        ];
+        $helperform = new HelperForm();
+        $helperform->table = $this->table;
+        $helperform->name_controller = $this->name;
+        $helperform->submit_action = 'submit' . $this->name;
+        $helperform->fields_value['slider_name'] = Configuration::get("slider_name");
+        $helperform->fields_value['input_slider'] = 'test';
+        return $helperform->generateForm([$form]);
+    }
+    public function displayInsertForm(){
+        $form=[
+            'form' =>[
+                'input' =>array(
+                    array(
+                        'type' => 'text',
+                        'name' => 'slider_name',
+                        'required' => true,
+                        'label' =>'Module_Name'
+                    ),
+                    array(
+                        'type'=>'text',
+                        'name' => 'title',
+                        'required' => true,
+                        'label' =>'title'
+                    ),
+                    array(
+                        'type'=>'text',
+                        'name' => 'position',
+                        'required' => true,
+                        'label' =>'position'
+                    ),
+                    array(
+                        'type'=>'text',
+                        'name' => 'status',
+                        'required' => true,
+                        'label' =>'status'
+                    ),
+
+                ),
+                'submit' =>[
+                    'title' => 'submit',
+                ]
+            ]
+           
+        ];
+        $helperform = new HelperForm();
+        $helperform->table = $this->table;
+        $helperform->name_controller = $this->name;
+        $helperform->submit_action = 'submit' . $this->name;
+        return $helperform->generateForm([$form]);
+    }
+    
 } 
 ?>
